26f69522cd51d56f0768ac076e60e9398360d191
SlimeyWimey <anis.koraichi@outlook.fr>
2021-03-09 19:29:28 +0100

    Correction légère de MapGenerator

--- Assets/Scripts/World/MapGenerator.cs
+++ Assets/Scripts/World/MapGenerator.cs
@@ -30,10 +30,6 @@ public class MapGenerator : MonoBehaviour
     public TerrainType[] regions;
 
 
-    private void Start()
-    {
-        DrawMapInEditor();
-    }
 
     /// <summary>
     /// Permet de charger le monde et ses textures lors du lancement
