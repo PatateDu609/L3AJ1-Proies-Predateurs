e66fa6f1c6e4a2518d6415848898878af416d1de
SlimeyWimey <anis.koraichi@outlook.fr>
2021-03-22 19:12:15 +0100

    Clouds and rain

---
+++ Assets/Scripts/Environment/Events/Rain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class Rain : MonoBehaviour
+{
+
+    public bool isRaining;
+
+
+
+    private void Update()
+    {
+        if (isRaining) GetComponentInChildren<ParticleSystem>().Play();
+        else GetComponentInChildren<ParticleSystem>().Stop();
+    }
+
+}
